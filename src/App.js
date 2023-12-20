20ca89a81e61953e3f4b4710751f3acb2e6943c0
0xv1n4y <vinaysurisetty02@gmail.com>
2023-12-20 16:46:15 +0530

    newVersion

--- src/App.js
+++ src/App.js
@@ -1,57 +1,49 @@
-import { useState,useEffect } from 'react';
-import axios from 'axios';
-import Header from './pages/Navbar';
-import Body from './pages/Body';
-
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
-  const [location,setLocation]=useState("")
-  const [weatherData,setWeatherData]=useState(false)
+  
+  const [weatherData, setWeatherData] = useState();
+  const [location, setLocation] = useState("");
 
   const API_KEY = "0306f3e1767baa02731f4d5bb9b8cff4";
-  const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
   //Fetch The weather Data in OpenweathermapAPI
 
-  const fetchWeatherData=async()=>{
-    try{
+  const fetchWeatherData = async () => {
+    try {
+      const { data } = await axios.get(API_URL, {
+        params: { q: location, appid: API_KEY, units: "metric" },
+      });
 
-      const response=await axios.get(API_URL,{params:{q:location,appid:API_KEY,units:"metric"}})
-      console.log(response.data)
-      setWeatherData(response.data)
-
-
-    }catch(err){
-      console.error("Error to Fetching Weather data",err)
+      setWeatherData(data);
+    } catch (err) {
+      console.error("Error to Fetching Weather data", err);
     }
-  
-  }
+  };
 
-  useEffect(()=>{
-    if(location){
-      fetchWeatherData()
+  useEffect(() => {
+    if (location) {
+      fetchWeatherData();
     }
-  },[location])
+  }, [location]);
 
+  const handlecheck = (e) => {
+    setLocation(e.target.value);
+  };
+  // To Reload the window
+  const reload = () => {
+    window.location.reload();
+  };
 
- const handlecheck=(e)=>{
-  setLocation(e.target.value)
- }
- // To Reload the window
- const reload=()=>{
-  window.location.reload()
- }
   return (
-
-      <div >
-
-          <Header reload={reload} location={location} handlecheck={handlecheck}/>
-          
-          <Body weatherData={weatherData}/>
-        
-      </div>
+    <div>
+      <Navbar reload={reload} location={location} handlecheck={handlecheck} />
+      <Main weatherData={weatherData} />
+    </div>
   );
 }
 
